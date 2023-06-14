7dbec2e2d9df941253b36c51de8098b56d1f59f8
____ <22010176@st.phenikaa-uni.edu.vn>
2023-06-14 11:08:18 +0700

    ed

--- de1/_2.c
+++ de1/_2.c
@@ -4,44 +4,32 @@
 #include <ctype.h>
 #include <conio.h>
 typedef struct {
-  int seat;
-  double price;
-  char* branch, * color;
+  int seat; double price;
+  char branch[10], color[5];
 } Xe;
-char* NhapMauXe(int x) {
-  switch (x) {
-  case 1: return "Den";
-  case 2: return "Do";
-  case 3: return "Xanh";
-  case 4: return "Trang";
-  case 5: return "Vang";
-  case 6: return "Bac";
-  default: return "";
-  }
-}
 int checkInt(int x) { return x > 0; }
 int checkDouble(double x) { return x > 0; }
 Xe* TaoXe(char* hang, char* mau, int seat, double gia) {
   Xe* _xe = malloc(sizeof(Xe));
   strcpy(_xe->branch, hang);
   strcpy(_xe->color, mau);
-  _xe->seat = seat;
-  _xe->price = gia;
+  _xe->seat = seat; _xe->price = gia;
   return _xe;
 }
 Xe* CopyXe(Xe _a) { return TaoXe(_a.branch, _a.color, _a.seat, _a.price); }
 Xe* NhapXe() {
-  char* hang = malloc(11 * sizeof(char));
-  char* _mau = malloc(6 * sizeof(char));
-  int seat, mau;
-  double a;
+  char* hang = malloc(11);
+  char* _mau = malloc(6);
+  int seat, mau; double gia;
   do {
     printf("Nhap hang xe: ");
     fflush(stdin); gets(hang);
   } while (strlen(hang) >= 10);
+
   do {
-    printf("Nhap mau xe(1-6): ");
-    fflush(stdin); scanf("%d", mau);
+    printf("Nhap mau xe (1-6): ");
+    fflush(stdin);
+    scanf("%d", &mau);
     switch (mau) {
     case 1:
       strcpy(_mau, "Den");
@@ -66,7 +54,6 @@ Xe* NhapXe() {
 
   do {
     printf("Nhap cho ngoi xe (>0): ");
-    printf("Nhap cho ngoi xe (>0): ");
     fflush(stdin);
     scanf("%d", &seat);
   } while (seat < 0);
@@ -74,10 +61,10 @@ Xe* NhapXe() {
   do {
     printf("Nhap gia xe (>0): ");
     fflush(stdin);
-    scanf("%lf", &a);
-  } while (a < 0);
+    scanf("%lf", &gia);
+  } while (gia < 0);
 
-  return TaoXe(hang, _mau, seat, a);
+  return TaoXe(hang, _mau, seat, gia);
 }
 void InXe(Xe* _a) {
   printf("%s %s %d %lf", _a->branch, _a->color, _a->seat, _a->price);
